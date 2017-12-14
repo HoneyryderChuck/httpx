9ea8e6f309ea9607d1fd680037b9dcda18e5bb42
HoneyryderChuck <cardoso_tiago@hotmail.com>
2017-12-14 11:10:21 +0000

    removed keep alive explicit timeout (it's just part of the operation flow)

--- lib/httpx/timeout/per_operation.rb
+++ lib/httpx/timeout/per_operation.rb
@@ -4,18 +4,15 @@ require "timeout"
 
 module HTTPX::Timeout
   class PerOperation < Null
-    KEEP_ALIVE_TIMEOUT = 5
     OPERATION_TIMEOUT = 5
     CONNECT_TIMEOUT = 5
 
-    attr_reader :connect_timeout, :operation_timeout, :keep_alive_timeout
+    attr_reader :connect_timeout, :operation_timeout
 
     def initialize(connect: CONNECT_TIMEOUT,
-                   operation: OPERATION_TIMEOUT,
-                   keep_alive: KEEP_ALIVE_TIMEOUT)
+                   operation: OPERATION_TIMEOUT)
       @connect_timeout = connect
       @operation_timeout = operation
-      @keep_alive_timeout = keep_alive
       @timeout = @connect_timeout
     end
 
@@ -28,8 +25,7 @@ module HTTPX::Timeout
     def ==(other)
       other.is_a?(PerOperation) &&
       @connect_timeout == other.connect_timeout &&
-      @operation_timeout == other.operation_timeout &&
-      @keep_alive_timeout == other.keep_alive_timeout
+      @operation_timeout == other.operation_timeout
     end
   end
 end
