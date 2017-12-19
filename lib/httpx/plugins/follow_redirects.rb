c96e5b0a50957adf7c859c2e982a4e6d22dbb433
HoneyryderChuck <cardoso_tiago@hotmail.com>
2017-12-19 15:37:37 +0200

    refactored the redirect module; allow to pass an already initialized body to the request

--- lib/httpx/plugins/follow_redirects.rb
+++ lib/httpx/plugins/follow_redirects.rb
@@ -19,21 +19,16 @@ module HTTPX
             @keep_open = true
             
             max_redirects = @default_options.max_redirects || MAX_REDIRECTS
-            responses = Array(super)
+            requests = __build_reqs(*args, **options)
+            responses = __send_reqs(*requests)
 
             loop do 
               redirect_requests = []
               indexes = responses.each_with_index.map do |response, index|
                 next unless REDIRECT_STATUS.include?(response.status)
-
-
-                redirect_uri = URI(response.headers["location"])
-                redirect_uri = response.uri.merge(redirect_uri) if redirect_uri.relative?
-
-                # TODO: integrate cookies in the next request
-                # redirects are **ALWAYS** GET
-                request = __build_req(:get, redirect_uri, options)
-                redirect_requests << request
+                request = requests[index]
+                retry_request = __build_redirect_req(request, response, options)
+                redirect_requests << retry_request
                 index
               end.compact
               break if redirect_requests.empty?
@@ -42,6 +37,7 @@ module HTTPX
 
               redirect_responses = __send_reqs(*redirect_requests)
               indexes.each_with_index do |index, i2|
+                requests[index] = redirect_requests[i2]
                 responses[index] = redirect_responses[i2]
               end 
             end
@@ -51,6 +47,19 @@ module HTTPX
           ensure
             @keep_open = keep_open
           end
+        end
+
+        private
+
+        def __build_redirect_req(request, response, options)
+          redirect_uri = URI(response.headers["location"])
+          redirect_uri = response.uri.merge(redirect_uri) if redirect_uri.relative?
+
+          # TODO: integrate cookies in the next request
+          # redirects are **ALWAYS** GET
+          retry_options = options.merge(headers: request.headers,
+                                        body: request.body)
+          __build_req(:get, redirect_uri, retry_options)
         end
       end
 
