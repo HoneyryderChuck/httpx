9ee00921f80c690877ea472d192ca8d71eccb71d
HoneyryderChuck <cardoso_tiago@hotmail.com>
2017-12-20 17:26:57 +0200

    added proxy tests; the tests use local nghttpx as proxy, which means this'll become a dependency

---
+++ test/support/requests/plugins/proxy.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Requests
+  module Plugins
+    module Proxy
+
+      def test_plugin_proxy_anonymous
+        server = proxy_server(proxy_uri: proxy_uri)
+
+        server.run do
+          client = HTTPX.plugin(:proxy).with_proxy(proxy_uri: proxy_uri)
+          uri = build_uri("/")
+          response = client.get(uri)
+          verify_status(response.status, 200)
+          verify_body_length(response)
+        end
+      end
+
+
+
+      private
+
+      def proxy_uri
+        "http://127.0.0.1:9999"
+      end
+
+      def proxy_server(**args)
+        ProxyServer.new(**args)
+      end
+    end
+  end
+end 
