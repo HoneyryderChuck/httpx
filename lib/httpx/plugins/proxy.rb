defe140466b35ed7d7be8dd2bd556ac02b38fbd8
HoneyryderChuck <cardoso_tiago@hotmail.com>
2017-12-20 19:00:21 +0200

    proxy: http-proxying is now possible; on connection, a proxy channel is initialized, which should match against ALL requests, so this means that, by passing a proxy, one is saying that ALL requests on that client are proxied; a lot of object monkey-patching takes place, but this should be alright, as these objects should be long-lived in the grand scheme of things (if there will only be one channel, there will also be only one parser, etc etc)

--- lib/httpx/plugins/proxy.rb
+++ lib/httpx/plugins/proxy.rb
@@ -6,22 +6,42 @@ module HTTPX
   module Plugins
     module Proxy
       module ConnectionMethods
+
         private
 
-        def build_channel(uri)
-          return super unless @options.proxy
+        def build_proxy_channel
+          raise "must have proxy defined" unless @options.proxy
           parameters = Parameters.new(**@options.proxy)
           io = TCP.new(parameters.uri, @options)
-          channel = ProxyChannel.new(io, @options) do |request, response|
+          ProxyChannel.new(io, @options) do |request, response|
             @responses[request] = response
           end 
-          connect_request = Request.new(:connect, uri)
-          if parameters.authenticated?
-            connect_request.headers["proxy-authentication"] = parameters.token_authentication
-          end
-          channel.send(connect_request)
-          register_channel(channel)
-          channel 
+
+          # parameters = Parameters.new(**@options.proxy)
+          # unless uri.scheme == "https"
+          #   @options.headers["proxy-connection"] = "keep-alive"
+          #   return super(parameters.uri)
+          # end
+
+          # connect_request = ProxyRequest.new(uri)
+          # connect_request.headers["proxy-connection"] = "keep-alive"
+          # if parameters.authenticated?
+          #   connect_request.headers["proxy-authentication"] = parameters.token_authentication
+          # end
+          # channel.send(connect_request)
+          # register_channel(channel)
+          # channel 
+        end
+      end
+
+      module ParserMethods
+        def headline(request)
+          "#{request.verb.to_s.upcase} #{request.uri.to_s} HTTP/#{@version.join(".")}"
+        end
+
+        def set_request_headers(request)
+          super
+          request.headers["proxy-connection"] = request.headers["connection"] 
         end
       end
 
@@ -29,6 +49,10 @@ module HTTPX
         def initialize(*)
           super
           @connection.extend(ConnectionMethods)
+          if not @default_options.proxy.empty?
+            channel = @connection.__send__(:build_proxy_channel)
+            @connection.__send__(:register_channel, channel)
+          end
         end
 
         def with_proxy(*args)
@@ -45,29 +69,51 @@ module HTTPX
       @connected = false
     end
 
-    private
-
-    def parser
-      return super if @connected
-      @parser || begin
-      @parser = HTTP1.new(@write_buffer, @options.merge(max_concurrent_requests: 1))
-        @parser.once(:response, &method(:on_connect))
-        @parser.on(:close) { throw(:close, self) }
-        @parser
-      end
+    def match?(*)
+      true
     end
 
-    def on_connect(request, response)
-      if response.status == 200
-        @connected = true
-        @parser.close
-        @parser = nil 
-      else
-        pending = @parser.instance_variable_get(:@pending)
-        while req = pending.shift
-          @on_response.call(req, response)
-        end
-      end
+    def parser
+      pr = super
+      pr.extend(Plugins::Proxy::ParserMethods)
+      pr
+    end
+
+    private
+
+    #def parser
+    #  return super if @connected
+    #  @parser || begin
+    #  @parser = HTTP1.new(@write_buffer, @options.merge(max_concurrent_requests: 1))
+    #    @parser.once(:response, &method(:on_connect))
+    #    @parser.on(:close) { throw(:close, self) }
+    #    @parser
+    #  end
+    #end
+
+    #def on_connect(request, response)
+    #  if response.status == 200
+    #    @connected = true
+    #    @parser.close
+    #    @parser = nil 
+    #  else
+    #    pending = @parser.instance_variable_get(:@pending)
+    #    while req = pending.shift
+    #      @on_response.call(req, response)
+    #    end
+    #  end
+    #end
+  end
+
+  class ProxyRequest < Request
+    def initialize(uri, options = {})
+      super(:connect, uri, options)
+      @headers.delete("user-agent")
+      @headers.delete("accept")
+    end
+
+    def path
+      "#{@uri.host}:#{@uri.port}"
     end
   end
 
