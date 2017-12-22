08f018c2487a1ebea56bc81b7ca1fd30de45cb56
HoneyryderChuck <cardoso_tiago@hotmail.com>
2017-12-22 09:53:11 +0200

    fixed proxy script

--- examples/proxy_get.rb
+++ examples/proxy_get.rb
@@ -3,14 +3,14 @@ require "httpx"
 include HTTPX
 
 # supports HTTP/1 pipelining and HTTP/2
-URLS  = %w[https://nghttp2.org https://nghttp2.org/blog/]# * 3
+URLS  = %w[http://nghttp2.org https://nghttp2.org/blog/]# * 3
 
 client = HTTPX.plugin(:proxy)
-# client = client.with_proxy(proxy_uri: "http://139.162.116.181:51089")
-# responses = client.get(URLS)
-# puts responses.map(&:status)
+client = client.with_proxy(uri: "http://61.7.174.110:54132")
+responses = client.get(URLS)
+puts responses.map(&:status)
 
-response = client.get(URLS.first)
-puts response.status
+# response = client.get(URLS.first)
+# puts response.status
 
 
