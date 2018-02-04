728d5e9bc6ec73a07348ad79d1f1c36206075008
HoneyryderChuck <cardoso_tiago@hotmail.com>
2018-02-04 00:11:35 +0000

    namespacing loggable

--- lib/httpx/loggable.rb
+++ lib/httpx/loggable.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-module Loggable
-  def log(level = @options.debug_level, label = "", &msg)
-    return unless @options.debug
-    return unless @options.debug_level >= level
-    @options.debug << (+label << msg.call << "\n")
+module HTTPX
+  module Loggable
+    def log(level = @options.debug_level, label = "", &msg)
+      return unless @options.debug
+      return unless @options.debug_level >= level
+      @options.debug << (+label << msg.call << "\n")
+    end
   end
 end
