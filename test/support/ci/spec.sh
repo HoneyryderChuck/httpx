36fd0bef881cc958644e12d37ef5779f6a7c15db
HoneyryderChuck <cardoso_tiago@hotmail.com>
2018-02-09 19:27:24 +0000

    added docker setup with dante/squid (for proxies)

---
+++ test/support/ci/spec.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cleanup () {
+  docker-compose -p ci kill
+  docker-compose -p ci rm -f --all
+}
+
+docker build -t httpx -f test/support/ci/Dockerfile .
+
+docker-compose -f test/support/ci/docker-compose.yml -p ci build
+docker-compose -f test/support/ci/docker-compose.yml -p ci up -d
+
+#
+#
+# TEST_EXIT_CODE=`docker wait ci_httpx_1`
+docker run --env PARALLEL=1 COVERAGE=1 httpx rake test
+
+trap cleanup exit
