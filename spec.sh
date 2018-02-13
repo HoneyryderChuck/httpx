e5a588a98a40d31713aa359b89014c07145f36cf
HoneyryderChuck <cardoso_tiago@hotmail.com>
2018-02-13 21:37:30 +0000

    moved spec.sh to root

--- spec.sh
+++ spec.sh
@@ -8,11 +8,13 @@ cleanup () {
 docker build -t httpx -f test/support/ci/Dockerfile .
 
 docker-compose -f test/support/ci/docker-compose.yml -p ci build
-docker-compose -f test/support/ci/docker-compose.yml -p ci up -d
+docker-compose -f test/support/ci/docker-compose.yml -p ci up httpx \
+  --exit-code-from httpx \
+  --abort-on-container-exit
 
 #
 #
 # TEST_EXIT_CODE=`docker wait ci_httpx_1`
-docker run --env PARALLEL=1 COVERAGE=1 httpx rake test
+# docker run --env PARALLEL=1 COVERAGE=1 httpx rake test
 
 trap cleanup exit
