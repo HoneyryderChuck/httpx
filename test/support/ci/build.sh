bc6f3ea85b5e86f9aafbb20771657b55b77bc763
HoneyryderChuck <cardoso_tiago@hotmail.com>
2018-02-18 14:17:13 +0000

    indent

--- test/support/ci/build.sh
+++ test/support/ci/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 apk update && apk upgrade
 apk add --no-cache g++ make git bash
-cd /home && touch Gemfile.lock && rm Gemfile.lock && bundle install && bundle exec rake test:ci
+cd /home && touch Gemfile.lock && \
+  rm Gemfile.lock && \
+  bundle install && \
+  bundle exec rake test:ci
 
 
