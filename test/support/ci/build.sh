722b577eb3bd8ac4fed17501287f5b701b2cf681
HoneyryderChuck <cardoso_tiago@hotmail.com>
2018-02-19 23:35:27 +0000

    only run jekyll for 2.5

--- test/support/ci/build.sh
+++ test/support/ci/build.sh
@@ -4,8 +4,15 @@ apk add --no-cache g++ make git bash
 cd /home && touch Gemfile.lock && \
   rm Gemfile.lock && \
   bundle install && \
-  bundle exec rake test:ci && \
+  bundle exec rake test:ci
+
+
+RUBY_VERSION=$(ruby -e 'puts RUBY_VERSION')
+
+if [[ $RUBY_VERSION = "2.5"* ]] ;
+then
   cd www && bundle install && \
   bundle exec jekyll build -d public
+fi
 
 
