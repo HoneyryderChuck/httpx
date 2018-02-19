eb3da2bca9789343a0e88f84d2bb062351818257
HoneyryderChuck <cardoso_tiago@hotmail.com>
2018-02-19 00:52:37 +0000

    moved jekyll task to the container (needs ruby)

--- test/support/ci/build.sh
+++ test/support/ci/build.sh
@@ -4,6 +4,8 @@ apk add --no-cache g++ make git bash
 cd /home && touch Gemfile.lock && \
   rm Gemfile.lock && \
   bundle install && \
-  bundle exec rake test:ci
+  bundle exec rake test:ci && \
+  cd www && bundle install && \
+  bundle exec jekyll build -d ../public
 
 
