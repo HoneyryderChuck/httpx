df47e04e52d462eb49e53ca75bfbae9850e4fbcb
HoneyryderChuck <cardoso_tiago@hotmail.com>
2018-08-10 17:05:36 +0100

    moved parsing test to the resolver helpers

--- test/resolver/https_test.rb
+++ test/resolver/https_test.rb
@@ -30,6 +30,27 @@ class HTTPSResolverTest < Minitest::Test
     assert !resolver.empty?
     connection.verify
 
+  def test_parse_no_record
+    @has_error = false
+    resolver.on(:error) { @has_error = true }
+    channel = build_channel("https://idontthinkthisexists.org/")
+    resolver << channel
+    resolver.queries["idontthinkthisexists.org"] = channel
+
+    # this is only here to drain
+    write_buffer.clear
+    resolver.parse(no_record)
+    assert channel.addresses.nil?
+    assert resolver.queries.key?("idontthinkthisexists.org")
+    assert !@has_error, "resolver should still be able to resolve AAAA"
+    # A type
+    write_buffer.clear
+    resolver.parse(no_record)
+    assert channel.addresses.nil?
+    assert resolver.queries.key?("idontthinkthisexists.org")
+    assert @has_error, "resolver should have failed"
+  end
+
   def test_io_api
     __test_io_api
   end
@@ -45,6 +66,27 @@ class HTTPSResolverTest < Minitest::Test
   end
 
   def write_buffer
-    resolver.instance_variable_get(:@write_buffer)
+    resolver.instance_variable_get(:@resolver_channel)
+            .instance_variable_get(:@pending)
+  end
+
+  MockResponse = Struct.new(:headers, :body) do
+    def to_s ; body ; end
+  end
+
+  def a_record
+    MockResponse.new({'content-type' => 'application/dns-udpwireformat'}, super)
+  end
+  
+  def aaaa_record
+    MockResponse.new({'content-type' => 'application/dns-udpwireformat'}, super)
+  end
+
+  def cname_record
+    MockResponse.new({'content-type' => 'application/dns-udpwireformat'}, super)
+  end
+
+  def no_record
+    MockResponse.new({'content-type' => 'application/dns-udpwireformat'}, super)
   end
 end
--- test/resolver/native_test.rb
+++ test/resolver/native_test.rb
@@ -6,38 +6,7 @@ require_relative "../test_helper"
 class NativeResolverTest < Minitest::Test
   include ResolverHelpers
   include HTTPX
-
-  A_ANSWER = "\x00\x03\x81\x80\x00\x01\x00\x01\x00\x03\x00\x06\x04ipv4\x05tlund\x02se\x00\x00\x01\x00\x01\xC0\f\x00" \
-             "\x01\x00\x01\x00\x00\v\xC3\x00\x04\xC1\x0F\xE4\xC3\xC0\x11\x00\x02\x00\x01\x00\x00\v\xC3\x00\x0F\x02ns" \
-             "\x06agartz\x03net\x00\xC0\x11\x00\x02\x00\x01\x00\x00\v\xC3\x00\t\x02ns\x03nxs\xC0\x17\xC0\x11\x00\x02" \
-             "\x00\x01\x00\x00\v\xC3\x00\b\x05slave\xC0Y\xC0V\x00\x01\x00\x01\x00\x01M\xCF\x00\x04\xC1\x0F\xE4\xC2\xC0;" \
-             "\x00\x01\x00\x01\x00\x01M\xCF\x00\x04\xC1\x0F\xE4\xC2\xC0k\x00\x01\x00\x01\x00\x01M\xCF\x00\x04\xC1\x0F" \
-             "\xE4\xC2\xC0V\x00\x1C\x00\x01\x00\x01M\xCF\x00\x10 \x01\x06|\x18\x98\x02\x01\x00\x00\x00\x00\x00\x00\x00S" \
-             "\xC0;\x00\x1C\x00\x01\x00\x01M\xCF\x00\x10*\x00\b\x01\x00\x0F\x00\x00\x00\x00\x00\x00\x00\x00\x00S\xC0k\x00" \
-             "\x1C\x00\x01\x00\x01M\xCF\x00\x10*\x00\b\x01\x00\x0F\x00\x00\x00\x00\x00\x00\x00\x00\x00S".b
-
-  AAAA_ANSWER = "\x00\x02\x81\x80\x00\x01\x00\x01\x00\x03\x00\x06\x04" \
-                "ipv6\x05tlund\x02se\x00\x00\x1C\x00\x01\xC0\f\x00\x1C" \
-                "\x00\x01\x00\x00\x0E\x10\x00\x10*\x00\b\x01\x00\x0F\x00" \
-                "\x00\x00\x00\x00\x00\x00\x00\x01\x95\xC0\x11\x00\x02\x00\x01" \
-                "\x00\x00\r\xBE\x00\t\x02ns\x03nxs\xC0\x17\xC0\x11\x00\x02\x00" \
-                "\x01\x00\x00\r\xBE\x00\x0F\x02ns\x06agartz\x03net\x00\xC0\x11" \
-                "\x00\x02\x00\x01\x00\x00\r\xBE\x00\b\x05slave\xC0J\xC0G\x00\x01" \
-                "\x00\x01\x00\x01O\xCA\x00\x04\xC1\x0F\xE4\xC2\xC0\\\x00\x01\x00" \
-                "\x01\x00\x01O\xCA\x00\x04\xC1\x0F\xE4\xC2\xC0w\x00\x01\x00\x01\x00" \
-                "\x01O\xCA\x00\x04\xC1\x0F\xE4\xC2\xC0G\x00\x1C\x00\x01\x00\x01O\xCA" \
-                "\x00\x10 \x01\x06|\x18\x98\x02\x01\x00\x00\x00\x00\x00\x00\x00S\xC0\\" \
-                "\x00\x1C\x00\x01\x00\x01O\xCA\x00\x10*\x00\b\x01\x00\x0F\x00\x00\x00" \
-                "\x00\x00\x00\x00\x00\x00S\xC0w\x00\x1C\x00\x01\x00\x01O\xCA\x00\x10*" \
-                "\x00\b\x01\x00\x0F\x00\x00\x00\x00\x00\x00\x00\x00\x00S".b
-
-  CNAME_ANSWER = "\x00\x02\x81\x80\x00\x01\x00\x01\x00\x01\x00\x00\x05ipv4c\x05tlund\x02se" \
-                 "\x00\x00\x1C\x00\x01\xC0\f\x00\x05\x00\x01\x00\x00\a\x1F\x00\a\x04ipv4\xC0" \
-                 "\x12\xC0\x12\x00\x06\x00\x01\x00\x00\x01,\x00%\x02ns\x03nxs\xC0\x18\x05tlund" \
-                 "\xC0BxIv\x91\x00\x008@\x00\x00\x0E\x10\x00\e\xAF\x80\x00\x00\x01,".b
-
-  NO_RECORD_ANSWER = "\x00\x02\x81\x83\x00\x01\x00\x00\x00\x01\x00\x00\x14idontthinkthisexists\x03org\x00\x00\x1C\x00\x01\xC0!\x00\x06\x00\x01\x00\x00\x03\x84\x003\x02a0\x03org\vafilias-nst\x04info\x00\x03noc\xC0=w\xFD|\xD2\x00\x00\a\b\x00\x00\x03\x84\x00\t:\x80\x00\x01Q\x80"
-
+ 
   def test_append_ipv4
     super
     assert resolver.empty?
@@ -53,36 +22,12 @@ class NativeResolverTest < Minitest::Test
     assert resolver.empty?
   end
 
-  def test_append_external_name
+  def test_append_hostname
     channel = build_channel("https://news.ycombinator.com")
     resolver << channel
     assert channel.addresses.nil?, "there should be no direct IP"
-    assert !resolver.empty?
-    resolver.__send__(:resolve)
+    resolver.resolve
     assert !write_buffer.empty?, "there should be a DNS query ready to be sent"
-  end
-
-  def test_parse_a_record
-    channel = build_channel("http://ipv4.tlund.se/")
-    resolver.queries["ipv4.tlund.se"] = channel
-    resolver.parse(A_ANSWER)
-    assert channel.addresses.include?("193.15.228.195")
-  end
-
-  def test_parse_aaaa_record
-    channel = build_channel("http://ipv6.tlund.se/")
-    resolver.queries["ipv6.tlund.se"] = channel
-    resolver.parse(AAAA_ANSWER)
-    assert channel.addresses.include?("2a00:801:f::195")
-  end
-
-  def test_parse_cname_record
-    channel = build_channel("http://ipv4c.tlund.se/")
-    resolver.queries["ipv4c.tlund.se"] = channel
-    resolver.parse(CNAME_ANSWER)
-    assert channel.addresses.nil?
-    assert !resolver.queries.key?("ipv4c.tlund.se")
-    assert resolver.queries.key?("ipv4.tlund.se")
   end
 
   def test_parse_no_record
@@ -90,22 +35,18 @@ class NativeResolverTest < Minitest::Test
     resolver.on(:error) { @has_error = true }
     channel = build_channel("https://idontthinkthisexists.org/")
     resolver << channel
+    resolver.resolve
     resolver.queries["idontthinkthisexists.org"] = channel
 
     # this is only here to drain
-    resolver.parse(NO_RECORD_ANSWER)
-    assert channel.addresses.nil?
-    assert resolver.queries.key?("idontthinkthisexists.org")
-    assert !@has_error, "resolver should still be able to resolve AAAA"
     write_buffer.clear
-    # AAAA type
-    resolver.parse(NO_RECORD_ANSWER)
+    resolver.parse(no_record)
     assert channel.addresses.nil?
     assert resolver.queries.key?("idontthinkthisexists.org")
     assert !@has_error, "resolver should still be able to resolve A"
-    write_buffer
     # A type
-    resolver.parse(NO_RECORD_ANSWER)
+    write_buffer.clear
+    resolver.parse(no_record)
     assert channel.addresses.nil?
     assert resolver.queries.key?("idontthinkthisexists.org")
     assert @has_error, "resolver should have failed"
