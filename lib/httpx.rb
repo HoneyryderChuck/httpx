e051ea57da475756c8f3ee377f9d49335dc0d5d2
HoneyryderChuck <cardoso_tiago@hotmail.com>
2018-11-01 02:20:21 +0000

    added alternative services store

--- lib/httpx.rb
+++ lib/httpx.rb
@@ -5,6 +5,7 @@ require "httpx/version"
 require "httpx/extensions"
 
 require "httpx/errors"
+require "httpx/altsvc"
 require "httpx/callbacks"
 require "httpx/loggable"
 require "httpx/registry"
---
+++ lib/httpx/altsvc.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module HTTPX
+  module AltSvc 
+    @lookup_mutex = Mutex.new
+    @lookups = Hash.new { |h, k| h[k] = [] }
+
+    module_function
+
+    def cached_lookup(origin)
+      now = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      @lookup_mutex.synchronize do
+        lookup(origin, now)
+      end
+    end
+
+    def cached_lookup_set(origin, entry)
+      now = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      entry["TTL"] = Integer(entry["ma"]) + now if entry.key?("ma")
+      @lookup_mutex.synchronize do
+        return if @lookups[origin].any? { |svc| svc["origin"] == entry["origin"] }
+        @lookups[origin] << entry
+        entry
+      end
+    end
+
+    def lookup(origin, ttl)
+      return unless @lookups.key?(origin)
+      @lookups[origin] = @lookups[origin].select do |entry|
+        !entry.key?("TTL") || entry["TTL"] > ttl
+      end
+      @lookups[origin].select do |entry|
+        !entry["noop"]
+      end.find do |entry|
+        entry["origin"] == origin
+      end
+    end
+  end
+end
