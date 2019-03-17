d6893c79bab4d11a32414488b9fda299af6870e3
HoneyryderChuck <cardoso_tiago@hotmail.com>
2019-03-17 01:43:40 +0000

    example on how to use httpx with faraday

---
+++ examples/stripe_faraday.rb
@@ -0,0 +1,24 @@
+require "httpx/adapters/faraday"
+require "stripe"
+
+Stripe.api_key = "sk_test_123"
+Stripe.api_base = "https://localhost:12112"
+
+conn = Faraday.new do |builder|
+  builder.use Faraday::Request::Multipart
+  builder.use Faraday::Request::UrlEncoded
+  builder.use Faraday::Response::RaiseError
+
+  builder.adapter :httpx
+end
+conn.ssl.verify_mode = 0
+
+client = Stripe::StripeClient.new(conn)
+
+response = Stripe::Charge.create({
+  amount: 100,
+  currency: "usd",
+  source: "src_123"
+}, {client: client})
+
+puts "response: #{response.status}"
