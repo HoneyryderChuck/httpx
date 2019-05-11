e87f484dd8ea35b8823cfe2cfb12a05a2fdd49d6
HoneyryderChuck <cardoso_tiago@hotmail.com>
2019-05-11 16:09:35 +0300

    faraday adapter uses peristent plugin by default

--- lib/httpx/adapters/faraday.rb
+++ lib/httpx/adapters/faraday.rb
@@ -24,6 +24,7 @@ module Faraday
 
       class Session < ::HTTPX::Session
         plugin(:compression)
+        plugin(:persistent)
 
         module ReasonPlugin
           if RUBY_VERSION < "2.5"
