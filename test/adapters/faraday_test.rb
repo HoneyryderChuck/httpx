ca2e2523d947d71264c1f7ed30e6013e11521d13
HoneyryderChuck <cardoso_tiago@hotmail.com>
2019-05-17 11:53:05 +0300

    added adapter prefix to faraday tests

--- test/adapters/faraday_test.rb
+++ test/adapters/faraday_test.rb
@@ -13,7 +13,7 @@ class FaradayTest < Minitest::Test
 
   def_delegators :create_connection, :get, :head, :put, :post, :patch, :delete, :run_request
 
-  def test_in_parallel
+  def test_adapter_in_parallel
     resp1, resp2 = nil, nil
 
     connection = create_connection
@@ -29,12 +29,12 @@ class FaradayTest < Minitest::Test
     assert_equal "OK", resp2.reason_phrase
   end
 
-  def test_get_handles_compression
+  def test_adapter_get_handles_compression
     res = get(build_path("/gzip"))
     assert JSON.parse(res.body.to_s)["gzipped"]
   end
 
-  def test_get_ssl_fails_with_bad_cert
+  def test_adapter_get_ssl_fails_with_bad_cert
     fake_store = OpenSSL::X509::Store.new
     conn = create_connection(ssl: { cert_store: fake_store, verify_mode: OpenSSL::SSL::VERIFY_PEER })
     err = assert_raises Faraday::SSLError do
@@ -43,42 +43,42 @@ class FaradayTest < Minitest::Test
     assert_includes err.message, "certificate"
   end
 
-  def test_get_send_url_encoded_params
+  def test_adapter_get_send_url_encoded_params
     assert_equal({ "name" => "zack" }, JSON.parse(get(build_path("/get"), name: "zack").body.to_s)["args"])
   end
 
-  def test_get_retrieves_the_response_headers
+  def test_adapter_get_retrieves_the_response_headers
     response = get(build_path("/get"))
     assert_match(%r{application/json}, response.headers["Content-Type"], "original case fail")
     assert_match(%r{application/json}, response.headers["content-type"], "lowercase fail")
   end
 
-  def test_get_sends_user_agent
+  def test_adapter_get_sends_user_agent
     response = get(build_path("/user-agent"), { name: "user-agent" }, user_agent: "Agent Faraday")
     assert_equal "Agent Faraday", JSON.parse(response.body.to_s)["user-agent"]
   end
 
-  def test_get_reason_phrase
+  def test_adapter_get_reason_phrase
     response = get(build_path("/get"))
     assert_equal "OK", response.reason_phrase
   end
 
-  def test_post_send_url_encoded_params
+  def test_adapter_post_send_url_encoded_params
     json = JSON.parse post(build_path("/post"), name: "zack").body
     assert_equal({ "name" => "zack" }, json["form"])
   end
 
-  def test_post_send_url_encoded_nested_params
+  def test_adapter_post_send_url_encoded_nested_params
     resp = post(build_path("/post"), "name" => { "first" => "zack" })
     json = JSON.parse resp.body.to_s
     assert_equal({ "name[first]" => "zack" }, json["form"])
   end
 
-  def test_post_retrieves_the_response_headers
+  def test_adapter_post_retrieves_the_response_headers
     assert_match(%r{application/json}, post(build_path("/post")).headers["content-type"])
   end
 
-  def test_post_sends_files
+  def test_adapter_post_sends_files
     resp = post(build_path("/post")) do |req|
       req.body = { "uploaded_file" => Faraday::UploadIO.new(__FILE__, "text/x-ruby") }
     end
@@ -88,46 +88,46 @@ class FaradayTest < Minitest::Test
     assert_equal(json["files"]["uploaded_file"].bytesize, File.size(__FILE__))
   end
 
-  def test_put_send_url_encoded_params
+  def test_adapter_put_send_url_encoded_params
     json = JSON.parse put(build_path("/put"), name: "zack").body.to_s
     assert_equal({ "name" => "zack" }, json["form"])
   end
 
-  def test_put_send_url_encoded_nested_params
+  def test_adapter_put_send_url_encoded_nested_params
     resp = put(build_path("/put"), "name" => { "first" => "zack" })
     json = JSON.parse resp.body.to_s
     assert_equal({ "name[first]" => "zack" }, json["form"])
   end
 
-  def test_put_retrieves_the_response_headers
+  def test_adapter_put_retrieves_the_response_headers
     assert_match(%r{application/json}, put(build_path("/put")).headers["content-type"])
   end
 
-  def test_patch_send_url_encoded_params
+  def test_adapter_patch_send_url_encoded_params
     json = JSON.parse patch(build_path("/patch"), name: "zack").body.to_s
     assert_equal({ "name" => "zack" }, json["form"])
   end
 
-  def test_head_retrieves_no_response_body
+  def test_adapter_head_retrieves_no_response_body
     assert_equal "", head(build_path("/get")).body.to_s
   end
 
-  def test_head_retrieves_the_response_headers
+  def test_adapter_head_retrieves_the_response_headers
     assert_match(%r{application/json}, head(build_path("/get")).headers["content-type"])
   end
 
-  def test_delete_retrieves_the_response_headers
+  def test_adapter_delete_retrieves_the_response_headers
     assert_match(%r{application/json}, delete(build_path("/delete")).headers["content-type"])
   end
 
-  # def test_timeout
+  # def test_adapter_timeout
   #   conn = create_connection(request: { timeout: 1, open_timeout: 1 })
   #   assert_raises Faraday::Error::TimeoutError do
   #     conn.get(build_path("/delay/5"))
   #   end
   # end
 
-  def test_connection_error
+  def test_adapter_connection_error
     assert_raises Faraday::Error::ConnectionFailed do
       get "http://localhost:4"
     end
