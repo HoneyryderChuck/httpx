5ff5fc38bc98ba267cd5de260f5048b72057bf84
HoneyryderChuck <cardoso_tiago@hotmail.com>
2019-12-22 01:55:02 +0000

    fixed ruby 2.3 lack of support for SSL_CERT_FILE (openssl)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,3 +16,7 @@ require "httpx"
 
 Dir[File.join(".", "test", "support", "*.rb")].each { |f| require f }
 Dir[File.join(".", "test", "support", "**", "*.rb")].each { |f| require f }
+
+# Ruby 2.3 openssl configuration somehow ignores SSL_CERT_FILE env var.
+# This adds it manually.
+OpenSSL::SSL::SSLContext::DEFAULT_CERT_STORE.add_file(ENV["SSL_CERT_FILE"]) if RUBY_VERSION.start_with?("2.3") && ENV.key?("SSL_CERT_FILE")
