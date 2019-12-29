fc65972c21cbfe1b5e22ebfda0ae5134726b8e36
HoneyryderChuck <cardoso_tiago@hotmail.com>
2019-12-29 02:42:38 +0000

    removed timeout patch which was messing with the connect timeout test

--- test/support/minitest_extensions.rb
+++ test/support/minitest_extensions.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
-# testing proxies is a drag...
-module TestTimeoutDefaults
-  def new(**)
-    timeout = super
-    timeout.instance_variable_set(:@connect_timeout, 5)
-    timeout
-  end
-end
-
-HTTPX::Timeout.extend(TestTimeoutDefaults)
-
 module MinitestExtensions
   module TimeoutForTest
     # our own subclass so we never confused different timeouts
