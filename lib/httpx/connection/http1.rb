0a0313b69dbf0c772ac226fde4d207792805c33b
HoneyryderChuck <cardoso_tiago@hotmail.com>
2020-03-07 22:09:09 +0000

    fixed http1 connection close recovery

--- lib/httpx/connection/http1.rb
+++ lib/httpx/connection/http1.rb
@@ -14,7 +14,7 @@ module HTTPX
     def initialize(buffer, options)
       @options = Options.new(options)
       @max_concurrent_requests = @options.max_concurrent_requests
-      @max_requests = Float::INFINITY
+      @max_requests = @max_concurrent_requests
       @parser = Parser::HTTP1.new(self)
       @buffer = buffer
       @version = [1, 1]
@@ -23,6 +23,7 @@ module HTTPX
     end
 
     def reset
+      @max_requests = @max_concurrent_requests
       @parser.reset!
     end
 
@@ -46,8 +47,7 @@ module HTTPX
     end
 
     def send(request)
-      if @max_requests.positive? &&
-         @requests.size >= @max_concurrent_requests
+      if !@max_requests.positive?
         @pending << request
         return
       end
