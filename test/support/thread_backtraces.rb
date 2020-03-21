00360e1ddf7c1d1c0df5961675fcb99d6b18f132
HoneyryderChuck <cardoso_tiago@hotmail.com>
2020-03-21 17:55:26 +0000

    removed invalid with_date_of_birth

--- test/support/thread_backtraces.rb
+++ test/support/thread_backtraces.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-BACKTRACE_HANDLER = lambda do
+Signal.trap("USR2") do
+  warn "starting..."
   Thread.list.each do |thread|
     warn "Thread TID-#{(thread.object_id ^ ::Process.pid).to_s(36)} #{thread.name}"
     if thread.backtrace
@@ -10,5 +11,3 @@ BACKTRACE_HANDLER = lambda do
     end
   end
 end
-
-Signal.trap("USR2", BACKTRACE_HANDLER)
