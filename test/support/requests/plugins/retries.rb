af2524cf6ee144a1e5e5372100889c924ccc7186
HoneyryderChuck <cardoso_tiago@hotmail.com>
2020-04-18 17:40:29 +0100

    abstracted retry request verification to an helper method

--- test/support/requests/plugins/retries.rb
+++ test/support/requests/plugins/retries.rb
@@ -6,14 +6,14 @@ module Requests
       def test_plugin_retries
         no_retries_session = HTTPX.plugin(RequestInspector).with_timeout(total_timeout: 3)
         no_retries_response = no_retries_session.get(build_uri("/delay/10"))
-        assert no_retries_response.is_a?(HTTPX::ErrorResponse)
+        assert verify_error_response(no_retries_response)
         assert no_retries_session.calls.zero?, "expect request to be built 1 times (was #{no_retries_session.calls})"
         retries_session = HTTPX
                           .plugin(RequestInspector)
                           .plugin(:retries)
                           .with_timeout(total_timeout: 3)
         retries_response = retries_session.get(build_uri("/delay/10"))
-        assert retries_response.is_a?(HTTPX::ErrorResponse)
+        assert verify_error_response(retries_response)
         assert retries_session.calls == 3, "expect request to be built 3 times (was #{retries_session.calls})"
       end
 
@@ -42,7 +42,8 @@ module Requests
                           .with_timeout(total_timeout: 3)
                           .max_retries(2)
         retries_response = retries_session.get(build_uri("/delay/10"))
-        assert retries_response.is_a?(HTTPX::ErrorResponse)
+
+        assert verify_error_response(retries_response)
         # we're comparing against max-retries + 1, because the calls increment will happen
         # also in the last call, where the request is not going to be retried.
         assert retries_session.calls == 2, "expect request to be built 2 times (was #{retries_session.calls})"
@@ -50,7 +51,7 @@ module Requests
 
       def test_plugin_retries_retry_on
         retry_callback = lambda do |response|
-          !response.error.is_a?(HTTPX::TimeoutError)
+          response.is_a?(HTTPX::ErrorResponse) && !response.error.is_a?(HTTPX::TimeoutError)
         end
 
         retries_session = HTTPX
@@ -60,7 +61,7 @@ module Requests
                           .max_retries(2)
 
         retries_response = retries_session.get(build_uri("/delay/10"))
-        assert retries_response.is_a?(HTTPX::ErrorResponse)
+        assert verify_error_response(retries_response)
         assert retries_session.calls.zero?, "expect request not to be retried (it was, #{retries_session.calls} times)"
       end
 
@@ -75,7 +76,7 @@ module Requests
         after_time = Process.clock_gettime(Process::CLOCK_MONOTONIC, :second)
         total_time = after_time - before_time
 
-        assert retries_response.is_a?(HTTPX::ErrorResponse)
+        assert verify_error_response(retries_response)
         assert_in_delta 3 + 2 + 3, total_time, 1, "request didn't take as expected to retry (#{total_time} secs)"
       end
 
@@ -92,8 +93,14 @@ module Requests
         after_time = Process.clock_gettime(Process::CLOCK_MONOTONIC, :second)
         total_time = after_time - before_time
 
-        assert retries_response.is_a?(HTTPX::ErrorResponse)
+        assert verify_error_response(retries_response)
         assert_in_delta 3 + 2 + 3 + 4 + 3, total_time, 1, "request didn't take as expected to retry (#{total_time} secs)"
+      end
+
+      private
+
+      def verify_error_response(response)
+        assert response.is_a?(HTTPX::ErrorResponse), "expected an error response, instead got #{response.inspect}"
       end
 
       module RequestInspector
