5083186654bbb4d01f90ba5fa3a6518213d4049f
HoneyryderChuck <cardoso_tiago@hotmail.com>
2021-01-03 18:12:35 +0000

    do not overwrite RUBYOPT (because of jit values)

--- test/support/ci/build.sh
+++ test/support/ci/build.sh
@@ -32,7 +32,7 @@ fi
 bundle install --quiet
 
 if [[ ${RUBY_VERSION:0:1} = "3" ]]; then
-  export RUBYOPT='-rbundler/setup -rrbs/test/setup'
+  export RUBYOPT='$RUBYOPT -rbundler/setup -rrbs/test/setup'
   export RBS_TEST_RAISE=true
   export RBS_TEST_LOGLEVEL=error
   export RBS_TEST_OPT='-Isig -ruri -rjson -ripaddr -rhttp-2-next'
