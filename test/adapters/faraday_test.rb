e253a702706619557845fa23257fcc308b08c3bf
HoneyryderChuck <cardoso_tiago@hotmail.com>
2021-01-31 03:46:28 +0000

    test bad certs errors with badssl.com instead

--- test/adapters/faraday_test.rb
+++ test/adapters/faraday_test.rb
@@ -34,11 +34,9 @@ class FaradayTest < Minitest::Test
     assert JSON.parse(res.body.to_s)["gzipped"]
   end
 
-  SYSTEM_CERT_STORE_DIR = "/usr/share/ca-certificates/mozilla"
   def test_adapter_get_ssl_fails_with_bad_cert
-    conn = create_connection(ssl: { ca_path: SYSTEM_CERT_STORE_DIR, verify: OpenSSL::SSL::VERIFY_PEER })
     err = assert_raises Faraday::Adapter::HTTPX::SSL_ERROR do
-      conn.get(build_path("/get"))
+      get("https://wrong.host.badssl.com/")
     end
     assert_includes err.message, "certificate"
   end
