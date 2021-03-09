00faafddc9aeac7152e1687359165cf9b70d5a18
HoneyryderChuck <cardoso_tiago@hotmail.com>
2021-03-09 19:29:17 +0000

    added tests around the tcp connect errors

--- test/support/requests/errors.rb
+++ test/support/requests/errors.rb
@@ -3,11 +3,31 @@
 module Requests
   module Errors
     def test_errors_connection_refused
-      skip if RUBY_ENGINE == "jruby"
       unavailable_host = URI(origin("localhost"))
       unavailable_host.port = next_available_port
       response = HTTPX.get(unavailable_host.to_s)
       verify_error_response(response, /Connection refused| not available/)
+    end
+
+    def test_errors_host_unreachable
+      uri = URI(origin("localhost")).to_s
+      return unless uri.start_with?("http://")
+
+      response = HTTPX.get(uri, addresses: [EHOSTUNREACH_HOST] * 2)
+      verify_error_response(response, /Host is unreachable/)
+    end
+
+    def test_errors_host_etimedout
+      uri = URI(origin("etimedout:#{ETIMEDOUT_PORT}")).to_s
+      return unless uri.start_with?("http://")
+
+      server = TCPServer.new("127.0.0.1", ETIMEDOUT_PORT)
+      begin
+        response = HTTPX.get(uri, addresses: %w[127.0.0.1] * 2)
+        verify_error_response(response, /Operation timed out/)
+      ensure
+        server.close
+      end
     end
 
     private
