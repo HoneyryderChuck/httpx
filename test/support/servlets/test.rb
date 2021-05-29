9a34af9e27b16352397b64bea858e82eb5e615ee
HoneyryderChuck <cardoso_tiago@hotmail.com>
2021-05-29 15:50:23 +0100

    moved test servlets to separate folder

---
+++ test/support/servlets/test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "logger"
+
+class TestServer < WEBrick::HTTPServer
+  def initialize(options = {})
+    super({
+      :BindAddress => "127.0.0.1",
+      :Port => 0,
+      :AccessLog => File.new(File::NULL),
+      :Logger => Logger.new(File::NULL),
+    }.merge(options))
+  end
+
+  def origin
+    sock = listeners.first
+    _, sock, ip, _ = sock.addr
+    "http://#{ip}:#{sock}"
+  end
+end
