d59137e90d7804216a18ecfde260468e558e634a
HoneyryderChuck <cardoso_tiago@hotmail.com>
2021-07-01 02:05:49 +0300

    fixing sort issue on truffleruby

--- lib/httpx/plugins/cookies/cookie.rb
+++ lib/httpx/plugins/cookies/cookie.rb
@@ -43,7 +43,7 @@ module HTTPX
         # Precedence: 1. longer path  2. older creation
         (@name <=> other.name).nonzero? ||
           (other.path.length <=> @path.length).nonzero? ||
-          (@created_at <=> other.created_at).nonzero?
+          (@created_at <=> other.created_at).nonzero? || 0
       end
 
       class << self
