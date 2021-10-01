c2131c8e41e6ff708f41495a771cdf3b66430da4
HoneyryderChuck <cardoso_tiago@hotmail.com>
2021-10-01 18:00:34 +0100

    disable ssl verify for nghttp2, letsencrypt expired cert + old openssl, thx

--- test/support/requests/plugins/push_promise.rb
+++ test/support/requests/plugins/push_promise.rb
@@ -4,7 +4,8 @@ module Requests
   module Plugins
     module PushPromise
       def test_plugin_no_push_promise
-        html, css = HTTPX.get(push_html_uri, push_css_uri, max_concurrent_requests: 1, http2_settings: { settings_enable_push: 1 })
+        html, css = no_push_promise_client.get(push_html_uri, push_css_uri, max_concurrent_requests: 1,
+                                                                            http2_settings: { settings_enable_push: 1 })
         verify_status(html, 200)
         verify_status(css, 200)
         verify_no_header(css.headers, "x-http2-push")
@@ -13,7 +14,7 @@ module Requests
       end
 
       def test_plugin_push_promise_get
-        session = HTTPX.plugin(:push_promise)
+        session = push_promise_client
         html, css = session.get(push_html_uri, push_css_uri)
         verify_status(html, 200)
         verify_status(css, 200)
@@ -24,8 +25,7 @@ module Requests
       end
 
       def test_plugin_push_promise_concurrent
-        session = HTTPX.plugin(:push_promise)
-                       .with(max_concurrent_requests: 100)
+        session = push_promise_client.with(max_concurrent_requests: 100)
         html, css = session.get(push_html_uri, push_css_uri)
         verify_status(html, 200)
         verify_status(css, 200)
@@ -36,6 +36,24 @@ module Requests
       end
 
       private
+
+      if RUBY_VERSION.start_with?("2.3")
+        def no_push_promise_client
+          HTTPX.with(ssl: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+        end
+
+        def push_promise_client
+          no_push_promise_client.plugin(:push_promise)
+        end
+      else
+        def no_push_promise_client
+          HTTPX
+        end
+
+        def push_promise_client
+          HTTPX.plugin(:push_promise)
+        end
+      end
 
       def push_origin
         "https://nghttp2.org"
