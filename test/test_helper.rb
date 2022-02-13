1fd9cbc83e03f65c0952b10f40afc50c91da4e98
HoneyryderChuck <cardoso_tiago@hotmail.com>
2022-02-13 15:02:54 +0000

    tests: disable auto-compaction for mimemagic-related tests
    mimemagic seems to corrupt memory under auto-compaction, so disabling it
    until it's fixed.
    
    https://github.com/mimemagicrb/mimemagic/issues/166

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-GC.auto_compact = true if GC.respond_to?(:auto_compact=)
+GC.auto_compact = true if !defined?(MimeMagic) && GC.respond_to?(:auto_compact=) # https://github.com/mimemagicrb/mimemagic/issues/166
 
 if ENV.key?("CI")
   require "simplecov"
