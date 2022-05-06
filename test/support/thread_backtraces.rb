e3191f0d6c29f22bba45f5cbef4fbe7e69626431
HoneyryderChuck <cardoso_tiago@hotmail.com>
2022-05-06 17:58:13 +0100

    rubocop lint

--- test/support/thread_backtraces.rb
+++ test/support/thread_backtraces.rb
@@ -4,10 +4,6 @@ Signal.trap("USR2") do
   warn "starting..."
   Thread.list.each do |thread|
     warn "Thread TID-#{(thread.object_id ^ ::Process.pid).to_s(36)} #{thread.name}"
-    if thread.backtrace
-      warn thread.backtrace
-    else
-      warn "<no backtrace available>"
-    end
+    warn thread.backtrace || "<no backtrace available>"
   end
 end
