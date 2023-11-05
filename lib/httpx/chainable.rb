885bf947b57a267e60b5539ae4ca56d1717a4e42
HoneyryderChuck <cardoso_tiago@hotmail.com>
2023-11-05 11:09:02 +0000

    deprecating callback methods on raw sessions

--- lib/httpx/chainable.rb
+++ lib/httpx/chainable.rb
@@ -45,22 +45,52 @@ module HTTPX
       Session.new(options, &blk)
     end
 
-    def method_missing(meth, *args, **options)
-      return super unless meth =~ /\Awith_(.+)/
+    def method_missing(meth, *args, **options, &blk)
+      case meth
+      when /\Awith_(.+)/
 
-      option = Regexp.last_match(1)
+        option = Regexp.last_match(1)
 
-      return super unless option
+        return super unless option
 
-      with(option.to_sym => args.first || options)
+        with(option.to_sym => args.first || options)
+      when /\Aon_(.+)/
+        callback = Regexp.last_match(1)
+
+        return super unless %w[
+          connection_opened connection_closed
+          request_error
+          request_started request_body_chunk request_completed
+          response_started response_body_chunk response_completed
+        ].include?(callback)
+
+        warn "DEPRECATION WARNING: calling `.#{meth}` on plain HTTPX sessions is deprecated. " \
+             "Use HTTPX.plugin(:callbacks).#{meth} instead."
+
+        plugin(:callbacks).__send__(meth, *args, **options, &blk)
+      else
+        super
+      end
     end
 
     def respond_to_missing?(meth, *)
-      return super unless meth =~ /\Awith_(.+)/
+      case meth
+      when /\Awith_(.+)/
+        option = Regexp.last_match(1)
 
-      option = Regexp.last_match(1)
+        default_options.respond_to?(option) || super
+      when /\Aon_(.+)/
+        callback = Regexp.last_match(1)
 
-      default_options.respond_to?(option) || super
+        %w[
+          connection_opened connection_closed
+          request_error
+          request_started request_body_chunk request_completed
+          response_started response_body_chunk response_completed
+        ].include?(callback) || super
+      else
+        super
+      end
     end
   end
 end
