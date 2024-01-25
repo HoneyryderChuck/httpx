dd0473e7cf3e059f9b21eb8213683b06f8d96113
HoneyryderChuck <cardoso_tiago@hotmail.com>
2024-01-25 00:05:37 +0000

    cleanly close the connection on HTTP2 protocol error
    when the HTTP/2 connection suffers an irrecovable error on the protocol level, it has to be terminated

--- lib/httpx/connection.rb
+++ lib/httpx/connection.rb
@@ -529,7 +529,7 @@ module HTTPX
       connecting? && callbacks_for?(:connect_error) ? emit(:connect_error, error) : handle_error(error)
       @state = :closed
       emit(:close)
-    rescue TLSError => e
+    rescue TLSError, HTTP2Next::Error::ProtocolError, HTTP2Next::Error::HandshakeError => e
       # connect errors, exit gracefully
       handle_error(e)
       connecting? && callbacks_for?(:connect_error) ? emit(:connect_error, e) : handle_error(e)
