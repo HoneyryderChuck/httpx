ffd20d73c82c84ea3db0dcb976eedf46793e8fb1
HoneyryderChuck <cardoso_tiago@hotmail.com>
2024-02-01 17:08:45 +0000

    remove needless usage of ||=

--- lib/httpx/plugins/oauth.rb
+++ lib/httpx/plugins/oauth.rb
@@ -64,10 +64,10 @@ module HTTPX
 
           metadata = http.get("#{@issuer}/.well-known/oauth-authorization-server").raise_for_status.json
 
-          @token_endpoint ||= metadata["token_endpoint"]
-          @scope ||= metadata["scopes_supported"]
-          @grant_type ||= Array(metadata["grant_types_supported"]).find { |gr| SUPPORTED_GRANT_TYPES.include?(gr) }
-          @token_endpoint_auth_method ||= Array(metadata["token_endpoint_auth_methods_supported"]).find do |am|
+          @token_endpoint = metadata["token_endpoint"]
+          @scope = metadata["scopes_supported"]
+          @grant_type = Array(metadata["grant_types_supported"]).find { |gr| SUPPORTED_GRANT_TYPES.include?(gr) }
+          @token_endpoint_auth_method = Array(metadata["token_endpoint_auth_methods_supported"]).find do |am|
             SUPPORTED_AUTH_METHODS.include?(am)
           end
           nil
