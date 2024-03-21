531a49ea569def100f4631a1674f6eade34dddd3
HoneyryderChuck <cardoso_tiago@hotmail.com>
2024-03-21 10:55:25 +0000

    rewrite http proxy layer to accomodate https proxies
    it should test both http ad https requests over an http proxy, and http and http2 requests over an https proxy

--- test/support/requests/plugins/proxy.rb
+++ test/support/requests/plugins/proxy.rb
@@ -19,14 +19,32 @@ module Requests
       end
 
       def test_plugin_http_http_proxy
-        return unless origin.start_with?("http://")
+        HTTPX.plugin(SessionWithPool)
+             .plugin(ProxyResponseDetector)
+             .plugin(:proxy, fallback_protocol: "http/1.1")
+             .with_proxy(uri: http_proxy).wrap do |session|
+          uri = build_uri("/get")
+          response = session.get(uri)
+          verify_status(response, 200)
+          verify_body_length(response)
+          assert response.proxied?
 
-        session = HTTPX.plugin(:proxy, fallback_protocol: "http/1.1").plugin(ProxyResponseDetector).with_proxy(uri: http_proxy)
-        uri = build_uri("/get")
-        response = session.get(uri)
-        verify_status(response, 200)
-        verify_body_length(response)
-        assert response.proxied?
+          conn = session.pool.connections.first
+          assert conn.io.is_a?(HTTPX::TCP)
+        end
+      end
+
+      def test_plugin_http_https_proxy
+        HTTPX.plugin(SessionWithPool).plugin(ProxyResponseDetector).plugin(:proxy).with_proxy(uri: https_proxy).wrap do |session|
+          uri = build_uri("/get")
+          response = session.get(uri)
+          verify_status(response, 200)
+          verify_body_length(response)
+          assert response.proxied?
+
+          conn = session.pool.connections.first
+          assert conn.io.is_a?(HTTPX::SSL)
+        end
       end
 
       def test_plugin_http_no_proxy
