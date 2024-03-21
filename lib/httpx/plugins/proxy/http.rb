00829ffde6d6148b08e16270900792c8b11b69e6
HoneyryderChuck <cardoso_tiago@hotmail.com>
2024-03-21 12:51:47 +0000

    adding support for SSL proxies
    SSL proxies (which have the "https://" scheme in the proxy URL) support
    is added, which requires a ruby-openssl version supporting "SSL in SSL"
    sockets.
    
    Closes https://github.com/HoneyryderChuck/httpx/issues/42

--- lib/httpx/plugins/proxy/http.rb
+++ lib/httpx/plugins/proxy/http.rb
@@ -91,7 +91,7 @@ module HTTPX
           end
 
           def handle_transition(nextstate)
-            return super unless @options.proxy && @options.proxy.uri.scheme == "http"
+            return super unless @options.proxy && @options.proxy.uri.scheme.start_with?("http")
 
             case nextstate
             when :connecting
