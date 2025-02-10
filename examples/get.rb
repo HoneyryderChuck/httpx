fd57d72a22108994900d2e03476a1cea449fd35f
HoneyryderChuck <cardoso_tiago@hotmail.com>
2025-02-10 22:32:31 +0000

    add support in get.rb script for arbitrary url

--- examples/get.rb
+++ examples/get.rb
@@ -1,11 +1,20 @@
 require "httpx"
 
-URLS  = %w[https://nghttp2.org/httpbin/get] * 1 
+if ARGV.empty?
+  URLS  = %w[https://nghttp2.org/httpbin/get] * 1
+else
+  URLS = ARGV
+end
 
 responses = HTTPX.get(*URLS)
-Array(responses).each(&:raise_for_status)
-puts "Status: \n"
-puts Array(responses).map(&:status)
-puts "Payload: \n"
-puts Array(responses).map(&:to_s)
-
+Array(responses).each do |res|
+  puts "URI: #{res.uri}"
+  case res
+  when HTTPX::ErrorResponse
+    puts "error: #{res.error}"
+    puts res.error.backtrace
+  else
+    puts "STATUS: #{res.status}"
+    puts res.to_s
+  end
+end
