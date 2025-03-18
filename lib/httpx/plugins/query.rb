7c59ef453763314282252f7ed1f02688297653f8
HoneyryderChuck <cardoso_tiago@hotmail.com>
2025-03-18 13:45:33 +0000

    added the :query plugin, which supports the QUERY http method
    added as a plugin for explicit opt-in, as it's still an experimental feature (RFC in draft)

---
+++ lib/httpx/plugins/query.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module HTTPX
+  module Plugins
+    #
+    # This plugin adds support for using the experimental QUERY HTTP method
+    #
+    # https://gitlab.com/os85/httpx/wikis/Query
+    module Query
+      def self.subplugins
+        {
+          retries: QueryRetries,
+        }
+      end
+
+      module InstanceMethods
+        def query(*uri, **options)
+          request("QUERY", uri, **options)
+        end
+      end
+
+      module QueryRetries
+        module InstanceMethods
+          private
+
+          def __repeatable_request?(request, options)
+            super || request.verb == "QUERY"
+          end
+        end
+      end
+    end
+
+    register_plugin :query, Query
+  end
+end
