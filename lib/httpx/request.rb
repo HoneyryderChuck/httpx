e2c60ae7e1a922fb51d654efb3f75530c8d9f2d4
HoneyryderChuck <cardoso_tiago@hotmail.com>
2025-08-12 15:23:41 +0100

    init nil reference on request for better opt shape

--- lib/httpx/request.rb
+++ lib/httpx/request.rb
@@ -103,7 +103,7 @@ module HTTPX
       raise UnsupportedSchemeError, "#{@uri}: #{@uri.scheme}: unsupported URI scheme" unless ALLOWED_URI_SCHEMES.include?(@uri.scheme)
 
       @state = :idle
-      @response = @peer_address = nil
+      @response = @peer_address = @context = @informational_status = nil
       @ping = false
       @persistent = @options.persistent
       @active_timeouts = []
