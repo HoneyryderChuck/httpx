8efdac53a4b86f4d4469482f6fe14e7cfd3e5047
HoneyryderChuck <cardoso_tiago@hotmail.com>
2025-10-21 11:19:02 +0100

    refactor test to use start_test_servlet helper

--- regression_tests/bug_0_14_4_test.rb
+++ regression_tests/bug_0_14_4_test.rb
@@ -7,9 +7,7 @@ class Bug_0_14_4_Test < Minitest::Test
   include HTTPHelpers
 
   def test_http1_keep_alive_persistent_requests_failed_to_start_new_connection_after_max_requests_reached
-    server = KeepAliveServer.new
-    th = Thread.new { server.start }
-    begin
+    start_test_servlet(KeepAliveServer) do |server|
       uri = "#{server.origin}/"
       uris = [uri] * 400
       HTTPX.plugin(SessionWithPool).with(max_requests: 100, max_concurrent_requests: 1).wrap do |http|
@@ -24,16 +22,11 @@ class Bug_0_14_4_Test < Minitest::Test
         connection_count = http.connection_count
         assert connection_count == 4, "expected to have 4 connections (+ an idle one), instead have #{connection_count}"
       end
-    ensure
-      server.shutdown
-      th.join
     end
   end
 
   def test_http1_keep_alive_persistent_requests_failed_to_start_new_connection_after_server_max_reached
-    server = KeepAliveServer.new
-    th = Thread.new { server.start }
-    begin
+    start_test_servlet(KeepAliveServer) do |server|
       uri = "#{server.origin}/2"
       uris = [uri] * 200
       HTTPX.plugin(SessionWithPool).with(max_requests: 100, max_concurrent_requests: 1).wrap do |http|
@@ -47,9 +40,6 @@ class Bug_0_14_4_Test < Minitest::Test
         connection_count = http.connection_count
         assert connection_count == 100, "expected to have 100 connections (+ an idle one), instead have #{connection_count}"
       end
-    ensure
-      server.shutdown
-      th.join
     end
   end
 end
