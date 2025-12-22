8d64b4326c1141e501c67b13686acbb43ca10254
HoneyryderChuck <cardoso_tiago@hotmail.com>
2025-12-22 12:21:03 +0000

    oauth: refactored to use auth plugin functionality
    the `:oauth` plugin has been majorly refactored internally, in order to
    reuse the `:auth` plugin functionality to manage the access token
    lifecycle.
    
    It introduces the `:oauth_options` option, which supersedes the values
    passed to `:oauth_session`; the latter is no longer an option passed
    around, but a "token manager" kept inside the session, which is used to
    associate access tokens with requests via the `:auth` plugin
    `generate_auth_token` callback.
    
    When used alongside the `:retries` plugin, sessions for which access
    tokens have expired (currently only validated through the 401 status
    code of initial request) will revalidate the token (via the
    refresh_token grant, whenever available) and retry the request.
    
    The `:oauth_session` option and `oauth_auth` and `with_access_token`
    methods have therefore been deprecated, as the token management
    described above should become "the way" in a future major version.

--- test/support/requests/plugins/oauth.rb
+++ test/support/requests/plugins/oauth.rb
@@ -3,73 +3,74 @@
 module Requests
   module Plugins
     module OAuth
-      def test_plugin_oauth_options
+      def test_plugin_oauth_oauth_session
         with_oauth_metadata do |server|
           # from options
-          opts = HTTPX.plugin(:oauth).oauth_auth(
+          oauth_session = HTTPX.plugin(:oauth).with_oauth_options(
             issuer: server.origin,
             client_id: "CLIENT_ID", client_secret: "SECRET",
             scope: "all"
-          ).instance_variable_get(:@options)
+          ).send(:oauth_session)
 
-          assert opts.oauth_session.grant_type == "client_credentials"
-          assert opts.oauth_session.token_endpoint.to_s == "#{server.origin}/token"
-          assert opts.oauth_session.token_endpoint_auth_method == "client_secret_basic"
-          assert opts.oauth_session.scope == %w[all]
-          assert opts.oauth_session.audience.nil?
+          assert oauth_session.token_endpoint == "#{server.origin}/token"
+          assert oauth_session.token_endpoint_auth_method == "client_secret_basic"
+          assert oauth_session.instance_variable_get(:@grant_type) == "client_credentials"
+          assert oauth_session.instance_variable_get(:@scope) == %w[all]
+          assert oauth_session.instance_variable_get(:@audience).nil?
 
           # with audience
-          opts = HTTPX.plugin(:oauth).oauth_auth(
+          oauth_session = HTTPX.plugin(:oauth).with_oauth_options(
             issuer: server.origin,
             client_id: "CLIENT_ID", client_secret: "SECRET",
             scope: "all",
             audience: "audience"
-          ).instance_variable_get(:@options)
+          ).send(:oauth_session)
 
-          assert opts.oauth_session.grant_type == "client_credentials"
-          assert opts.oauth_session.token_endpoint.to_s == "#{server.origin}/token"
-          assert opts.oauth_session.token_endpoint_auth_method == "client_secret_basic"
-          assert opts.oauth_session.scope == %w[all]
-          assert opts.oauth_session.audience == "audience"
+          assert oauth_session.token_endpoint.to_s == "#{server.origin}/token"
+          assert oauth_session.token_endpoint_auth_method == "client_secret_basic"
+          assert oauth_session.instance_variable_get(:@grant_type) == "client_credentials"
+          assert oauth_session.instance_variable_get(:@scope) == %w[all]
+          assert oauth_session.instance_variable_get(:@audience) == "audience"
 
           # from options, pointing to refresh
-          opts = HTTPX.plugin(:oauth).oauth_auth(
+          oauth_session = HTTPX.plugin(:oauth).with_oauth_options(
             issuer: "https://smthelse",
             token_endpoint_auth_method: "client_secret_post",
             client_id: "CLIENT_ID", client_secret: "SECRET",
             refresh_token: "REFRESH_TOKEN", access_token: "TOKEN",
             scope: %w[foo bar]
-          ).instance_variable_get(:@options)
-          assert opts.oauth_session.grant_type == "refresh_token"
-          assert opts.oauth_session.token_endpoint.to_s == "https://smthelse/token"
-          assert opts.oauth_session.token_endpoint_auth_method == "client_secret_post"
-          assert opts.oauth_session.scope == %w[foo bar]
+          ).send(:oauth_session)
+          assert oauth_session.token_endpoint.to_s == "https://smthelse/token"
+          assert oauth_session.token_endpoint_auth_method == "client_secret_post"
+          assert oauth_session.instance_variable_get(:@grant_type) == "refresh_token"
+          assert oauth_session.instance_variable_get(:@scope) == %w[foo bar]
 
           # from oauth server metadata url
-          opts = HTTPX.plugin(:oauth).oauth_auth(
+          session = HTTPX.plugin(:oauth).with_oauth_options(
             issuer: server.origin,
             client_id: "CLIENT_ID", client_secret: "SECRET",
-          ).with_access_token.instance_variable_get(:@options)
-          assert opts.oauth_session.grant_type == "client_credentials"
-          assert opts.oauth_session.token_endpoint.to_s == "#{server.origin}/token"
-          assert opts.oauth_session.token_endpoint_auth_method == "client_secret_basic"
-          assert opts.oauth_session.scope == %w[openid profile email address phone offline_access]
+          )
+          oauth_session = session.send(:oauth_session)
+          oauth_session.send(:load, session)
+
+          assert oauth_session.token_endpoint.to_s == "#{server.origin}/token"
+          assert oauth_session.token_endpoint_auth_method == "client_secret_basic"
+          assert oauth_session.instance_variable_get(:@grant_type) == "client_credentials"
+          assert oauth_session.instance_variable_get(:@scope) == %w[openid profile email address phone offline_access]
 
           # from hash
-          HTTPX.plugin(:oauth).with(
-            oauth_session: {
-              issuer: server.origin,
-              client_id: "CLIENT_ID", client_secret: "SECRET",
-              scope: "all"
-            }
-          ).instance_variable_get(:@options)
-          assert opts.oauth_session.grant_type == "client_credentials"
-          assert opts.oauth_session.token_endpoint.to_s == "#{server.origin}/token"
-          assert opts.oauth_session.token_endpoint_auth_method == "client_secret_basic"
-          assert opts.oauth_session.scope == %w[openid profile email address phone offline_access]
+          HTTPX.plugin(:oauth).with_oauth_options(
+            issuer: server.origin,
+            client_id: "CLIENT_ID", client_secret: "SECRET",
+            scope: "all"
+          ).send(:oauth_session)
+          assert oauth_session.token_endpoint.to_s == "#{server.origin}/token"
+          assert oauth_session.token_endpoint_auth_method == "client_secret_basic"
+          assert oauth_session.instance_variable_get(:@grant_type) == "client_credentials"
+          assert oauth_session.instance_variable_get(:@scope) == %w[openid profile email address phone offline_access]
 
           assert_raises(HTTPX::Error) do
-            HTTPX.plugin(:oauth).oauth_auth(
+            HTTPX.plugin(:oauth).with_oauth_options(
               issuer: server.origin,
               client_id: "CLIENT_ID", client_secret: "SECRET",
               token_endpoint_auth_method: "unsupported"
@@ -77,72 +78,141 @@ module Requests
           end
 
           assert_raises(HTTPX::Error) do
-            HTTPX.plugin(:oauth).oauth_auth(
+            HTTPX.plugin(:oauth).with_oauth_options(
               issuer: server.origin,
               client_id: "CLIENT_ID", client_secret: "SECRET",
               grant_type: "implicit_grant" # not supported
             )
           end
 
-          assert_raises(TypeError) do
-            HTTPX.plugin(:oauth).with(oauth_session: "wrong")
+          assert_raises(ArgumentError) do
+            HTTPX.plugin(:oauth).with_oauth_options("wrong")
           end
         end
       end
 
       def test_plugin_oauth_access_token_audience
         with_oauth_metadata do |server|
-          http = HTTPX.plugin(:oauth).oauth_auth(
-            issuer: server.origin,
-            client_id: "CLIENT_ID", client_secret: "SECRET",
-            scope: "all",
+          http = HTTPX.plugin(
+            :oauth,
+            oauth_options: {
+              issuer: server.origin,
+              client_id: "CLIENT_ID", client_secret: "SECRET",
+              scope: "all"
+            }
           )
-          http_aud = http.oauth_auth(
+          http_aud = http.with_oauth_options(
             issuer: server.origin,
             client_id: "CLIENT_ID", client_secret: "SECRET",
             scope: "all", audience: "audience"
           )
-          http_opts = http.with_access_token.instance_variable_get(:@options)
-          http_aud_opts = http_aud.with_access_token.instance_variable_get(:@options)
 
-          assert http_opts.oauth_session.access_token == "CLIENT-CREDS-AUTH"
-          assert http_aud_opts.oauth_session.access_token == "CLIENT-CREDS-AUTH-audience"
+          access_token = http.send(:oauth_session).fetch_access_token(http)
+          aud_access_token = http_aud.send(:oauth_session).fetch_access_token(http_aud)
+
+          assert access_token == "CLIENT-CREDS-AUTH"
+          assert aud_access_token == "CLIENT-CREDS-AUTH-audience"
         end
       end
 
       def test_plugin_oauth_client_credentials
         with_oauth_metadata do |server|
-          session = HTTPX.plugin(:oauth).oauth_auth(
-            issuer: server.origin,
-            client_id: "CLIENT_ID", client_secret: "SECRET", scope: "all"
+          session = HTTPX.plugin(
+            :oauth, oauth_options: {
+              issuer: server.origin,
+              client_id: "CLIENT_ID", client_secret: "SECRET", scope: "all"
+            }
           )
 
           client_creds_uri = build_uri("/client-credentials-authed", server.origin)
 
-          response = session.get(client_creds_uri)
+          response = HTTPX.get(client_creds_uri)
           verify_status(response, 401)
 
-          response = session.with_access_token.get(client_creds_uri)
+          response = session.get(client_creds_uri)
           verify_status(response, 200)
         end
       end
 
-      def test_plugin_oauth_refresh_token
+      def test_plugin_oauth_retries_refresh_token_on_retry
         with_oauth_metadata do |server|
-          session = HTTPX.plugin(:oauth).oauth_auth(
-            issuer: server.origin,
-            token_endpoint_auth_method: "client_secret_post",
-            client_id: "CLIENT_ID", client_secret: "SECRET",
-            refresh_token: "REFRESH_TOKEN", access_token: "TOKEN", scope: %w[foo bar]
+          session = HTTPX.plugin(:retries).plugin(
+            :oauth,
+            oauth_options: {
+              issuer: server.origin,
+              token_endpoint_auth_method: "client_secret_post",
+              client_id: "CLIENT_ID", client_secret: "SECRET",
+              refresh_token: "REFRESH_TOKEN", access_token: "TOKEN", scope: %w[foo bar]
+            }
           )
 
           refresh_token_uri = build_uri("/refresh-token-authed", server.origin)
 
-          response = session.get(refresh_token_uri)
+          response = HTTPX.get(refresh_token_uri)
           verify_status(response, 401)
 
-          response = session.with_access_token.get(refresh_token_uri)
+          response = session.get(refresh_token_uri)
           verify_status(response, 200)
+        end
+      end
+
+      def test_plugin_oauth_deprecated_oauth_session_option
+        with_oauth_metadata do |server|
+          oauth_session = nil
+          assert_output(nil, /DEPRECATION WARNING: option `:oauth_session` is deprecated/) do
+            # from options
+            oauth_session = HTTPX.plugin(:oauth).with_oauth_session(
+              issuer: server.origin,
+              client_id: "CLIENT_ID", client_secret: "SECRET",
+              scope: "all"
+            ).send(:oauth_session)
+          end
+
+          assert oauth_session.token_endpoint == "#{server.origin}/token"
+          assert oauth_session.token_endpoint_auth_method == "client_secret_basic"
+          assert oauth_session.instance_variable_get(:@grant_type) == "client_credentials"
+          assert oauth_session.instance_variable_get(:@scope) == %w[all]
+          assert oauth_session.instance_variable_get(:@audience).nil?
+        end
+      end
+
+      def test_plugin_oauth_deprecated_oauth_auth
+        with_oauth_metadata do |server|
+          oauth_session = nil
+          assert_output(nil, /DEPRECATION WARNING: `oauth_auth` is deprecated/) do
+            # from options
+            oauth_session = HTTPX.plugin(:oauth).oauth_auth(
+              issuer: server.origin,
+              client_id: "CLIENT_ID", client_secret: "SECRET",
+              scope: "all"
+            ).send(:oauth_session)
+          end
+
+          assert oauth_session.token_endpoint == "#{server.origin}/token"
+          assert oauth_session.token_endpoint_auth_method == "client_secret_basic"
+          assert oauth_session.instance_variable_get(:@grant_type) == "client_credentials"
+          assert oauth_session.instance_variable_get(:@scope) == %w[all]
+          assert oauth_session.instance_variable_get(:@audience).nil?
+        end
+      end
+
+      def test_plugin_oauth_deprecated_with_access_token
+        with_oauth_metadata do |server|
+          oauth_session = nil
+          assert_output(nil, /DEPRECATION WARNING: `with_access_token` is deprecated/) do
+            # from oauth server metadata url
+            session = HTTPX.plugin(:oauth).with_oauth_options(
+              issuer: server.origin,
+              client_id: "CLIENT_ID", client_secret: "SECRET",
+            )
+            oauth_session = session.with_access_token.send(:oauth_session)
+          end
+
+          assert oauth_session.token_endpoint == "#{server.origin}/token"
+          assert oauth_session.token_endpoint_auth_method == "client_secret_basic"
+          assert oauth_session.instance_variable_get(:@grant_type) == "client_credentials"
+          assert oauth_session.instance_variable_get(:@scope) == %w[openid profile email address phone offline_access]
+          assert oauth_session.access_token == "CLIENT-CREDS-AUTH"
         end
       end
 
