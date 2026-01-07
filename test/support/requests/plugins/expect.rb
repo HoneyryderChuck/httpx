2097d84c7fb5a1669e9f413fc3c1d85a20c78b2f
HoneyryderChuck <cardoso_tiago@hotmail.com>
2026-01-07 10:16:57 +0000

    moving 417 expect tests to standalone
    the proxy has been terminating sockets before the test can succeed when
    ran in parallel and sharing connections.

--- test/support/requests/plugins/expect.rb
+++ test/support/requests/plugins/expect.rb
@@ -62,16 +62,6 @@ module Requests
           verify_no_header(next_request.headers, "expect")
         end
       end
-
-      def test_plugin_expect_100_form_params_417
-        uri = build_uri("/status/417")
-        response = HTTPX.plugin(:expect).post(uri, form: { "foo" => "bar" })
-
-        # we can't really test that the request would be successful without it, however we can
-        # test whether the header has been removed from the request.
-        verify_status(response, 417)
-        verify_no_header(response.instance_variable_get(:@request).headers, "expect")
-      end
     end
   end
 end
