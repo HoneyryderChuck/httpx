d91d9ee56019aaa3362deaf40353e64a430c95a1
HoneyryderChuck <cardoso_tiago@hotmail.com>
2026-01-09 15:23:18 +0000

    added new timeout option: total_request_timeout
    this should be a timeout which stretches along interactions to get a final response, such as when they involve multiple retries or redirections

--- lib/httpx/request.rb
+++ lib/httpx/request.rb
@@ -100,7 +100,7 @@ module HTTPX
       @connection = @response =
         @drainer = @peer_address =
           @informational_status = @on_response_arrived = nil
-      @ping = false
+      @ping = @started = false
       @persistent = @options.persistent
       @active_timeouts = []
     end
@@ -142,6 +142,11 @@ module HTTPX
       @options.timeout[:request_timeout]
     end
 
+    # the total request timeout defined for this request.
+    def total_request_timeout
+      @options.timeout[:total_request_timeout]
+    end
+
     def persistent?
       @persistent
     end
@@ -165,6 +170,10 @@ module HTTPX
 
     def can_buffer?
       @state != :done
+    end
+
+    def started?
+      @started
     end
 
     # merges +h+ into the instance of HTTPX::Headers of the request.
@@ -287,6 +296,7 @@ module HTTPX
       when :headers
         return unless @state == :idle
 
+        @started = true
       when :body
         return unless @state == :headers ||
                       @state == :expect
