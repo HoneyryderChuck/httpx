ff2d223f7d4d353efbc752aaf21f3c2e67adc5b1
HoneyryderChuck <cardoso_tiago@hotmail.com>
2026-02-04 17:18:03 +0000

    retries: moved extraction of retry_after interval to internal method
    this allows the rate_limiter plugin to override the method without patching the option and potentially overriding a client value

--- lib/httpx/plugins/rate_limiter.rb
+++ lib/httpx/plugins/rate_limiter.rb
@@ -16,25 +16,7 @@ module HTTPX
 
       class << self
         def load_dependencies(klass)
-          klass.plugin(:retries, retry_after: method(:retry_after_rate_limit))
-        end
-
-        # Servers send the "Retry-After" header field to indicate how long the
-        # user agent ought to wait before making a follow-up request.  When
-        # sent with a 503 (Service Unavailable) response, Retry-After indicates
-        # how long the service is expected to be unavailable to the client.
-        # When sent with any 3xx (Redirection) response, Retry-After indicates
-        # the minimum time that the user agent is asked to wait before issuing
-        # the redirected request.
-        #
-        def retry_after_rate_limit(_, response)
-          return unless response.is_a?(Response)
-
-          retry_after = response.headers["retry-after"]
-
-          return unless retry_after
-
-          Utils.parse_retry_after(retry_after)
+          klass.plugin(:retries)
         end
       end
 
@@ -51,6 +33,24 @@ module HTTPX
 
         def rate_limit_error?(response)
           response.is_a?(Response) && RATE_LIMIT_CODES.include?(response.status)
+        end
+
+        # Servers send the "Retry-After" header field to indicate how long the
+        # user agent ought to wait before making a follow-up request.  When
+        # sent with a 503 (Service Unavailable) response, Retry-After indicates
+        # how long the service is expected to be unavailable to the client.
+        # When sent with any 3xx (Redirection) response, Retry-After indicates
+        # the minimum time that the user agent is asked to wait before issuing
+        # the redirected request.
+        #
+        def when_to_retry(_, response, options)
+          return super unless response.is_a?(Response)
+
+          retry_after = response.headers["retry-after"]
+
+          return super unless retry_after
+
+          Utils.parse_retry_after(retry_after)
         end
       end
     end
