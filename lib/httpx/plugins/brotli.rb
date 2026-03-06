41aa458ccde6b641a4b237125f68ddd3a593a892
Oliver Morgan <ollym@me.com>
2026-03-06 21:49:49 +0000

    Update brotli.rb

--- lib/httpx/plugins/brotli.rb
+++ lib/httpx/plugins/brotli.rb
@@ -57,12 +57,7 @@ module HTTPX
       end
 
       def self.extra_options(options)
-        supported_compression_formats = ["br", *options.supported_compression_formats].uniq
-
-        options.merge(
-          supported_compression_formats: supported_compression_formats,
-          headers: { "accept-encoding" => supported_compression_formats }
-        )
+        options.merge(supported_compression_formats: %w[br] + options.supported_compression_formats)
       end
 
       def encode(body)
