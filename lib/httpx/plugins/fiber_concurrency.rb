de38a84abd47a298f089d2f24f3c85cbb4708d43
HoneyryderChuck <cardoso_tiago@hotmail.com>
2026-03-18 15:06:39 +0000

    fiber concurrency: protect against spurious fiber scheduler IOError
    mitigation for https://bugs.ruby-lang.org/issues/21918
    
    It was observed that, since ruby 4.0, fibers may yield IOError due to a
    socket that has been closed, despite it not be the socket that is
    currently under monitoring, although it has been previously.
    
    because of this, IOError's are checked against in cases where expected,
    and it is assumed if the IO object isn't closed, then this was a
    spurious wakeup for a no-longer-nmonitored IO (it may be closed but if
    it has work to do, the same is implied).
    
    Solves https://github.com/HoneyryderChuck/httpx/issues/129

--- lib/httpx/plugins/fiber_concurrency.rb
+++ lib/httpx/plugins/fiber_concurrency.rb
@@ -76,11 +76,43 @@ module HTTPX
           super
         end
 
-        def send(request)
-          # DoH requests bypass the session, so context needs to be set here.
-          request.set_context!
+        def on_io_error(e)
+          return super unless e.is_a?(IOError) && e.message.include?("stream closed in another thread")
+
+          # @fiber-switch-guard
+          # sockets closed during fiber scheduler switches are raised in separate fibers than the fiber the
+          # socket may be used in. this check verifies that this is actually about this socket.
+          return unless to_io.closed?
+
+          if @state == :closing
+            # @fiber-switch-guard
+            # if the connection is reused across fibers, the socket may have been closed in the other fiber
+            # and switched here during the process, so continue what it was doing and transition to closed
+            # via #call.
+            call
+          elsif !backlog?
+            super
+          end
+        end
+
+        def on_connect_error(e)
+          return super unless e.is_a?(IOError) && e.message.include?("stream closed in another thread")
+
+          # @fiber-switch-guard
+          # sockets closed during fiber scheduler switches are raised in separate fibers than the fiber the
+          # socket may be used in. this check verifies that this is actually about this socket.
+          return unless to_io.closed? && !backlog?
 
           super
+        end
+
+        private
+
+        # checks whether the connection has any pending request (which the connection itself may
+        # have stored, or it may be somewhere in the parser).
+        def backlog?
+          @pending.any? ||
+            (@parser && (@parser.pending.any? || @parser.requests.any?))
         end
       end
 
@@ -165,6 +197,21 @@ module HTTPX
           return if @queries.empty?
 
           return unless @queries.values.any?(&:current_context?) || @connections.any?(&:current_context?)
+
+          super
+        end
+
+        def disconnect
+          return unless @connections.all?(&:current_context?)
+
+          super
+        end
+
+        def on_io_error(e)
+          # TODO: return super if this is not stream clsed in another thread
+
+          log { "IO Erroring: #{e.message}, current:#{@name}, queries:#{@queries.size}" }
+          return unless @name
 
           super
         end
