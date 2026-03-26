785052702a04dd8e2e4c0c14a70ed4a728d1e872
HoneyryderChuck <cardoso_tiago@hotmail.com>
2026-03-26 12:36:04 +0000

    revert thread passing on connection to pool checkin
    this has an adverse effect in multi-host multi-request situations, where in the context of a web server, retries or shutdown may be delayed because of premature context switching
    
    this is anyway a problem of the ruby thread scheduler which needs to be smarter, and would only benefit situations where the pool max connections is set and is way lower than the number of threads

--- lib/httpx/pool.rb
+++ lib/httpx/pool.rb
@@ -122,12 +122,6 @@ module HTTPX
 
         @max_connections_cond.signal
         @origin_conds[connection.origin.to_s].signal
-
-        # Observed situations where a session handling multiple requests in a loop
-        # across multiple threads checks the same connection in and out, while another
-        # thread which is waiting on the same connection never gets the chance to pick
-        # it up, because ruby's thread scheduler never switched on to it in the process.
-        Thread.pass
       end
     end
 
