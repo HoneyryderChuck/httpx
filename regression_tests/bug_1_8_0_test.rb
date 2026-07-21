a72d88e2d8ff548621ec1c333f81c21879f8af12
Richard Peng <richard@withpersona.com>
2026-07-20 17:05:49 -0700

    clear timer callbacks on connection close
    this helps prevent a resource leak when a remote closes a persistent
    connection

--- regression_tests/bug_1_8_0_test.rb
+++ regression_tests/bug_1_8_0_test.rb
@@ -13,4 +13,57 @@ class Bug_1_8_0_Test < Minitest::Test
 
     verify_error_response(response, HTTPX::ConnectTimeoutError)
   end
+
+  def test_plugin_retries_request_timeout_cancel_timers
+    with_unreliable_server do |uri|
+      http = HTTPX.plugin(:persistent).with(timeout: { request_timeout: 10 })
+
+      100.times do |i|
+        http.get(uri).raise_for_status
+      end
+
+      selector = Thread.current.thread_variable_get(:httpx_persistent_selector_store)[http]
+
+      callback_size = selector.
+        instance_variable_get(:@timers).
+        instance_variable_get(:@intervals).
+        map { |interval| interval.instance_variable_get(:@callbacks).size }.
+        sum
+
+      assert callback_size == 0, "Expected callbacks to be clear after close connection"
+    end
+  end
+
+  private
+
+  def with_unreliable_server
+    server = TCPServer.new(0)
+
+    thread = Thread.start do
+      loop do
+        begin
+          sock = server.accept
+          line = sock.gets until line != "\r\n"
+          sock.write "HTTP/1.1 200 OK\r\n" \
+                    "Content-Length: 2\r\n\r\n" \
+                    "ok"
+          sock.close
+        end
+      rescue IOError
+        break
+      end
+    end
+
+    begin
+      yield("http://localhost:#{server.addr[1]}")
+    ensure
+      begin
+        Timeout.timeout(2) do
+          server.close rescue nil
+        end
+      rescue Timeout::Error
+        thread.kill
+      end
+    end
+  end
 end
