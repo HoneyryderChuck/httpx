50e888639682722451ac77d35e7fcc1bce443493
HoneyryderChuck <cardoso_tiago@hotmail.com>
2026-08-07 16:33:27 +0100

    removed query tests check for 501 (now that CI nghttp2 aligned with public)

--- test/support/requests/plugins/query.rb
+++ test/support/requests/plugins/query.rb
@@ -3,8 +3,6 @@
 module Requests
   module Plugins
     module Query
-      QUERY_FAILED_STATUS_CODE = ENV.key?("CI") ? 501 : 405
-
       def test_plugin_query
         session = HTTPX.plugin(:query)
         assert session.respond_to?(:query)
@@ -12,7 +10,7 @@ module Requests
         uri = build_uri("/get")
 
         response = session.query(uri)
-        verify_status(response, QUERY_FAILED_STATUS_CODE) # not implemented yet
+        verify_status(response, 405) # not implemented yet
 
         request = response.instance_variable_get(:@request)
         assert request.verb == "QUERY"
@@ -20,12 +18,12 @@ module Requests
 
       def test_plugin_retries_query_can_be_retried
         check_error = ->(response) {
-          response.is_a?(HTTPX::ErrorResponse) || response.status == QUERY_FAILED_STATUS_CODE
+          response.is_a?(HTTPX::ErrorResponse) || response.status == 405
         }
         retries_session = HTTPX.plugin(RequestInspector).plugin(:query).plugin(:retries, retry_on: check_error)
         uri = build_uri("/get")
         retries_response = retries_session.query(uri)
-        verify_status(retries_response, QUERY_FAILED_STATUS_CODE) # not implemented yet
+        verify_status(retries_response, 405) # not implemented yet
         assert retries_session.calls == 3, "expect request to be built 3 times (was #{retries_session.calls})"
       end
     end
